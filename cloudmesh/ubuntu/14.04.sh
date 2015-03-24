37b31a25e3f3bb74da30cda8189799d45755919f
Badi' Abdul-Wahid <badi@iu.edu>
2015-03-24 16:48:12 -0400

    add webinstall script

---
+++ cloudmesh/ubuntu/14.04.sh
@@ -0,0 +1,25 @@
+VENV="$HOME/ENV"
+
+
+deps=($(curl raw.githubusercontent.com/cloudmesh/get/master/ | cut -d, -f3))
+
+if [[ ! ${deps[0]} -eq "ubuntu/14.04" ]]; then
+    # FIXME: bug report email address
+    echo "An unexpected error occurred."
+    echo "Please submit a bug"
+fi
+
+deps=${deps[@]:1}
+
+sudo apt-get update
+sudo apt-get -y install ${deps[@]}
+
+if test -d "$VENV"; then
+    echo "WARNING: $VENV already exists, using it"
+else
+    virtualenv ~/ENV
+fi
+source "$VENV"
+
+
+pip install cloudmesh
