f9fc3df2bd5e71f29bb14b9dd826b40a0de68c5b
Badi' Abdul-Wahid <badi@iu.edu>
2015-03-25 10:39:22 -0400

    checkout cloudmesh in order to ./install new (hack)

--- cloudmesh/ubuntu/14.04.sh
+++ cloudmesh/ubuntu/14.04.sh
@@ -30,3 +30,19 @@ source "$VENV"/bin/activate
 # due to circular dependencies, separate lines are needed
 pip install cloudmesh_base
 pip install cloudmesh
+
+# need to install files into ~/.cloudmesh
+cloudmesh_dir=cloudmesh.git
+install_extra_args=
+if test -d ~/.cloudmesh; then
+    echo "WARNING ~/.cloudmesh directory already exists, overwriting files"
+    install_extra_args='--force'
+fi
+
+
+test -d $cloudmesh_dir || git clone https://github.com/cloudmesh/cloudmesh.git $cloudmesh_dir
+cd $cloudmesh_dir
+git checkout dev2.0 # FIXME: remove on merge into master
+git pull
+./install new $install_extra_args
+
